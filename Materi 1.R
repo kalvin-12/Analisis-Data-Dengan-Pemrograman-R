13bae1058ba1b496884c9ced410ed4b3fba9623d
kalvin <VINIX@DEKSTOP-M61S7PN>
2024-02-11 00:38:13 +0700

    Modul 1

---
+++ Materi 1.R
@@ -0,0 +1,31 @@
+install.packages("tidyverse")
+library("tidyverse")
+
+
+View(penguins)
+
+install.packages('palmerpenguins')
+library('palmerpenguins')
+
+install.packages("ggplot2")
+library("ggplot2")
+
+
+#fungsi pertama 
+ggplot()
+
+ggplot(data=penguins,aes(x=flipper_length_mm,y=body_mass_g)) + geom_point
+
+ggplot(data=penguins,aes(x=flipper_length_mm,y=body_mass_g)) + geom_point(color="purple")
+
+ggplot(data=penguins,aes(x=flipper_length_mm,y=body_mass_g)) + geom_point(aes(color=species))
+
+ggplot(data=penguins,aes(x=flipper_length_mm,y=body_mass_g)) + geom_point(aes(shape=species,color=species))
+
+#fungsi facet memungkinkan kita membuat plot terpisah untuk setiap spesies
+
+ggplot(data=penguins,aes(x=flipper_length_mm,y=body_mass_g)) + geom_point(aes(shape=species,color=species)) + facet_grid("species")
+
+
+
+
