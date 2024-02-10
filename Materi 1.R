3516599fde4f8e71dc2f71e4282ef2372599d313
kalvin <VINIX@DEKSTOP-M61S7PN>
2024-02-11 00:50:12 +0700

    Update Modul 1

--- Materi 1.R
+++ Materi 1.R
@@ -23,9 +23,10 @@ ggplot(data=penguins,aes(x=flipper_length_mm,y=body_mass_g)) + geom_point(aes(co
 ggplot(data=penguins,aes(x=flipper_length_mm,y=body_mass_g)) + geom_point(aes(shape=species,color=species))
 
 #fungsi facet memungkinkan kita membuat plot terpisah untuk setiap spesies
-
 ggplot(data=penguins,aes(x=flipper_length_mm,y=body_mass_g)) + geom_point(aes(shape=species,color=species)) + facet_grid("species")
 
+#fungsi labs membuat judul
+ggplot(data=penguins,aes(x=flipper_length_mm,y=body_mass_g)) + geom_point(aes(shape=species,color=species)) + facet_grid("species") + labs(title = "Palmer Penguins : Body Mass vs Flipper Length")
 
 
 
