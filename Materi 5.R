00ef01509d7814314b2f9b97062bab2caa548448
kalvin <VINIX@DEKSTOP-M61S7PN>
2024-02-17 20:51:00 +0700

    Update Materi 5 (Final)

---
+++ Materi 5.R
@@ -0,0 +1,9 @@
+#materi R markdown
+#merupakan format file untuk membuat dokumen yang dinamis di R
+install.packages("rmarkdown")
+library(rmarkdown)
+
+#YMAL merupakan bagian header
+#bahasa untuk menerjemahkan data yang membuatnya lebih mudah dibaca
+
+
